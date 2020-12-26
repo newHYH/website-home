be4d2679d42f7203b1442f70a88725cf71b5c09f
yiboyue <yueyibo_ah@bonc.com.cn>
2020-12-26 16:45:27 +0800

    123

---
+++ src/components/map.vue
@@ -0,0 +1,12 @@
+<template>
+  <baidu-map class="map">
+  </baidu-map>
+</template>
+
+<style>
+/* 地图容器必须设置宽和高属性 */
+.map {
+  width: 400px;
+  height: 300px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,15 @@ const routes = [
   {
     path: '/home',
     component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
-  }
+  },
+  {
+    path: '/aftersale',
+    component: () => import(/* webpackChunkName: "about" */ '../views/aftersale.vue')
+  },
+  {
+    path: '/serviceNetwork',
+    component: () => import(/* webpackChunkName: "about" */ '../views/serviceNetwork.vue')
+  },
 ]
 
 const router = createRouter({
