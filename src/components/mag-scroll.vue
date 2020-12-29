5a063c9ddfa387ab14253a2fa6e9e9f17e4b5592
“lbking666666” <lbking666666@qq.com>
2020-12-29 17:01:11 +0800

    手机页面组件

--- src/components/mag-scroll.vue
+++ src/components/mag-scroll.vue
@@ -1,16 +1,19 @@
 <template>
-    <div class="mag-scroll-wrapper">
+    <div class="mag-scroll-wrapper" ref="scrollWrap">
         <div class="mag-scroll">
             <div class="title">
                 一眼相识 瞬间倾心
             </div>
+            <div class="g2"></div>
             <div class="scroll-wrapper" ref="scroll">
-                    <canvas class="canvas" ref="canvas" :width="canvasWidth" :height="canvasHeight"></canvas>
+                 <img :src="require('../assets/mag-img/img'+imgIndex+'.webp')" ref="canvas" class="canvas" alt="">
             </div>
+            <div class="g4"></div>
         </div>
     </div>
 </template>
 <script>
+    let canvas, cxt;
 export default {
     name: 'mag-scroll',
     props: {
@@ -18,8 +21,7 @@ export default {
     },
     data() {
         return {
-            canvasWidth: 0,
-            canvasHeight: 0,
+            imgIndex:0,
         }
     },
     mounted() {
@@ -27,8 +29,34 @@ export default {
     },
     methods: {
         init() {
-            this.canvasHeight = this.$refs.scroll.offsetHeight;
-            this.canvasWidth = this.$refs.scroll.offsetWidth;
+            window.addEventListener('scroll',this.handleScroll)
+        },
+        handleScroll(event){
+            let scrollTop = this.$refs.scrollWrap.offsetTop
+            let top = window.pageYOffset 
+            let scrollBottom = scrollTop + this.$refs.scrollWrap.offsetHeight
+
+            if(top >= scrollTop ){//判断在这个区域内
+                let distace = this.$refs.scrollWrap.offsetHeight - scrollTop
+                let stop = top-scrollTop
+                if(distace>=top){
+                    if(distace==top){
+                        this.imgIndex=1
+                    }else if(distace==top){
+                        this.imgIndex = 109
+                    }else{
+                       let n = parseInt(stop/(this.$refs.canvas.offsetHeight/109));
+                        if(n<=109){
+                            setTimeout(()=>{
+                                this.imgIndex = n
+                            },100)
+                           
+                        } 
+                    }   
+                }else{
+                     this.imgIndex = 109
+                }
+            }
         }
     }
 }
@@ -39,6 +67,14 @@ export default {
     height:200vh;
     position: relative;
     z-index: 1;
+    width: calc(82.05128vw + 4.61538px);
+    margin:0 auto;
+}
+.g2{
+    height:0.24rem;
+}
+.g4{
+    height:0.48rem;
 }
 .mag-scroll {
     height: calc(100vh - 88px);
@@ -57,12 +93,29 @@ export default {
         clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
          position: relative;
         flex:1;
+        display: flex;
     }
     .canvas{
         position: absolute;
         transform-style: preserve-3d;
         width:100%;
-        height:calc(1080 / 1920 * 100vw);
     }
 }
+@media screen and (max-width: 750px) {
+  .mag-scroll-wrapper{
+        width: 100%;
+        margin:0 auto;
+    }
+    .mag-scroll{
+        overflow: hidden;
+        img.canvas{
+            width:calc(1920/800*100vw);
+              height: calc(1080/800*100vw);
+              left:50%;
+              margin-left:calc(-1920/1600*100vw);
+              max-width:400% !important;
+        } 
+    }
+}
+
 </style>
