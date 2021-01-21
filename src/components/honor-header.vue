d9f1c3e5eab15fe1f7715aee18f87ba25326c4b9
unknown <2207252182@qq.com>
2021-01-21 10:07:35 +0800

    commit

---
+++ src/components/honor-header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="honor-header"></div>
+</template>
+
+<script>
+export default {
+    name: 'HonorHeader'
+}
+</script>
+
+<style lang="sass" scoped>
+@import "@/sass/common.scss";
+
+.honor-header{}
+</style>
