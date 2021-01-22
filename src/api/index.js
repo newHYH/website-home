60edaefef786216436bc0a9c6c812424fd4e94d0
“lbking666666” <lbking666666@qq.com>
2021-01-22 10:38:38 +0800

    离线地图

--- src/api/index.js
+++ src/api/index.js
@@ -2,27 +2,27 @@ import { getApi, postApi } from './config.js'
 
 //1.查询问题描述接口
 export function quryQuestion(data) {
-    const url = '/model/quryQuestion';
+    const url = '/customerservice/model/quryQuestion';
     return getApi(url, data)
 }
 //2.查询商品类型接口
 export function quryGoodsType(data) {
-    const url = '/model/quryGoodsType';
+    const url = '/customerservice/model/quryGoodsType';
     return getApi(url, data)
 }
 //3.门店查询接口
 export function quryDept(data) {
-    const url = '/model/quryDept';
+    const url = '/customerservice/model/quryDept';
     return postApi(url, data)
 }
 //4.sku商品信息查询接口
 export function quryGoodsSku(data) {
-    const url = '/model/quryGoodsSku';
+    const url = '/customerservice/model/quryGoodsSku';
     return postApi(url, data)
 }
 //5.sku商品类型树形信息查询接口
 export function quryGoodsTree(data) {
-    const url = '/model/quryGoodsTree';
+    const url = '/customerservice/model/quryGoodsTree';
     return getApi(url, data)
 }
 
