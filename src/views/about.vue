a8704067ff283d8b03c1b89d58d9be5e6e1dd418
“lbking666666” <lbking666666@qq.com>
2021-01-24 18:18:31 +0800

    404和关于我们

---
+++ src/views/about.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="about-wrapper">
+        <HeaderA />
+        <img src="../assets/about-banner.webp" class="about-banner" alt="">
+        <div class="about-box">
+            <div class="hd">
+                U-MAGIC
+            </div>
+            <div class="bd">
+                <h5>优先一步，畅享5G！</h5>
+                <p>U-MAGIC是中国联合网络通信有限公司于2021年推出的全新5G手机品牌，致力于为消费者打造集“优质网络、优异体验、优享权益”于一身的优秀5G产品，让消费者优先一步畅享5G。</p>
+            </div>
+        </div>
+        <Footer />
+    </div>
+</template>
+<script>
+import HeaderA from '@/components/headerA.vue';
+import Footer from '@/components/footer.vue';
+
+export default {
+    name: 'about',
+    components: {
+        // Header,
+        HeaderA,
+        Footer
+    },
+    data() {
+        return {}
+    },
+    mounted() {},
+    methods: {
+
+    }
+}
+</script>
+<style scoped lang=scss>
+@import "@/sass/common.scss";
+
+.about-wrapper {
+    width: 100%;
+    overflow: hidden;
+    .about-banner{
+        width:30%;
+        margin:0 auto;
+    }
+    .about-box {
+        margin: 0 auto;
+        width: 80%;
+        padding: 20px;
+
+        .hd {
+            font-size: 21px;
+            font-weight: 600;
+            color: #000;
+            position: relative;
+            padding-bottom: 2.2vw;
+
+            &:after {
+                content: '';
+                width: 3.2vw;
+                height: 1px;
+                background-color: #000000;
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                right: 0;
+                margin: 0 auto;
+            }
+        }
+
+        .bd {
+            padding:20px;
+            h5{
+                font-size:16px;
+                color:#000;
+                margin:15px 0;
+            }
+            p {
+                font-size: 1.08vw;
+                color: #000000;
+                line-height: 2.6vw;
+                max-width:400px;
+                margin:0 auto;
+            }
+        }
+    }
+
+
+}
+</style>
---
+++ src/views/notFound.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="not-wrapper">
+        <HeaderA />
+        <div class="not-box">
+            <img src="../assets/404.png" />
+            <div class="en_404">
+                <h4>这种错误可能因为：</h4>
+                <ul>
+                    <li>手抖输入错了~</li>
+                    <li>网络信号迷路了~</li>
+                    <li>内容被回收了~</li>
+                </ul>
+            </div>
+        </div>
+        <Footer />
+    </div>
+</template>
+<script>
+import HeaderA from '@/components/headerA.vue';
+import Footer from '@/components/footer.vue';
+
+export default {
+    name: 'notFound',
+    components: {
+        // Header,
+        HeaderA,
+        Footer
+    },
+    data() {
+        return {}
+    },
+    mounted() {},
+    methods: {
+
+    }
+}
+</script>
+<style scoped lang=scss>
+@import "@/sass/common.scss";
+
+.not-wrapper {
+    width: 100%;
+    overflow: hidden;
+
+
+    .not-box {
+        display: flex;
+        padding: 20px;
+        justify-content: center;
+
+        .en_404 {
+            padding: 50px;
+
+            h4 {
+                font-size: 1.041vw;
+                color:#000;
+                font-weight: 600;
+                margin-bottom: 0.521vw;
+            }
+
+            ul {
+
+                padding-left: 1.041vw;
+                color: #707070;
+
+                li {
+                    list-style: disc;
+                    font-size: 0.833vw;
+                    margin-bottom: 0.521vw;
+                    text-align: left;
+                }
+            }
+        }
+
+    }
+
+
+}
+</style>
