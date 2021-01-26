a4a681f56797b7b1f16b0f448e30d95adcf795a7
houyh <houge1422841169@qq.com>
2021-01-26 16:29:49 +0800

    新闻列表手机端适配

--- src/views/newsList.vue
+++ src/views/newsList.vue
@@ -4,20 +4,9 @@
         <div class="news-top">
             <img src="../assets/img_xinwen.png" alt="">
         </div>
-        <div class="newsList-wrap" v-for="(news,index) in newsList" :key="index">
-            <div class="news-box" @click="goto(news)">
-                <img src="../assets/xinwen_1.png" alt="">
-                <div class="news-content">
-                    <div class="news-time">
-                        {{timeFilter(news.creatTime)}} <span>NEW</span>
-                    </div>
-                    <div class="news-title">
-                        {{news.newsTitle}}
-                    </div>
-                </div>
-            </div>
-            <div class="news-box">
-                <img src="../assets/xinwen_1.png" alt="">
+        <div class="newsList-wrap">
+            <div class="news-box" v-for="(news,index) in newsList" :key="index" @click="goto(news)">
+                <img :src="news.cover" alt="">
                 <div class="news-content">
                     <div class="news-time">
                         {{timeFilter(news.creatTime)}} <span>NEW</span>
@@ -78,7 +67,7 @@
     @import "./../sass/common.scss";
     .newsList{
         width: 100%;
-        background: #fafafa;
+        background: #F5F5F5;
         .news-top{
             width: torem(1920);
             height: torem(850);
@@ -93,11 +82,13 @@
             display: flex;
             justify-content: space-between;
             font-size: torem(20);
+            flex-wrap: wrap;
             .news-box{
                 cursor: pointer;
                 width: torem(690);
                 height: torem(580);
                 position: relative;
+                margin-bottom: torem(59);
                 img{
                     position: absolute;
                     top: 0;
@@ -139,6 +130,78 @@
                         font-weight: 600;
                         color: #333333;
                         line-height: torem(33);
+                    }
+                }
+            }
+        }
+    }
+    @media screen and (max-width: 750px){
+        .newsList{
+            width: 100%;
+            background: #F5F5F5;
+            .news-top{
+                width: torem(1920);
+                height: torem(850);
+                img{
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+            .newsList-wrap{
+                width: 70%;
+                margin: 45px auto;
+                display: flex;
+                flex-direction: column;
+                flex-wrap: wrap;
+                justify-content: space-between;
+                .news-box{
+                    cursor: pointer;
+                    width: 100%;
+                    height: 60vw;
+                    position: relative;
+                    margin-bottom: 80px;
+                    img{
+                        position: absolute;
+                        top: 0;
+                        left: 0;
+                        width: 100%;
+                        height: 100%;
+                    }
+                    .news-content{
+                        width: 80%;
+                        height: 80px;
+                        background: #fff;
+                        color: #000;
+                        position: absolute;
+                        bottom: -10%;
+                        left: 10%;
+                        display: flex;
+                        flex-direction: column;
+                        justify-content: space-evenly;
+                        align-items: flex-start;
+                        padding: 10px;
+                        .news-time{
+                            font-size: 16px;
+                            font-weight: 400;
+                            color: #333333;
+                            line-height: 20px;
+                            span{
+                                font-size: 12px;
+                                width: 30px;
+                                height: 20px;
+                                line-height: 20px;
+                                background: #000000;
+                                border-radius: 10px;
+                                color: #fff;
+                                padding: 2px 5px;
+                            }
+                        }
+                        .news-title{
+                            font-size: 16px;
+                            font-weight: 600;
+                            color: #333333;
+                            line-height: 20px;
+                        }
                     }
                 }
             }
